83375a83f9da03296406518d5d9d3b1e6b5e81c0
Leif Andersen <leif@leifandersen.net>
2017-06-12 21:45:42 -0400

    Create actual packages.

---
+++ ffmpeg-i386-win32/info.rkt
@@ -0,0 +1,11 @@
+#lang info
+
+(define copy-foreign-lib
+  '("avcodec-57.dll"
+    "avdevice-57.dll"
+    "avfilter-6.dll"
+    "avformat-57.dll"
+    "avutil-55.dll"
+    "postproc-54.dll"
+    "swresample-2.dll"
+    "swscale-4.dll"))
---
+++ ffmpeg-x86_64-macosx/info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define copy-foreign-libs
+  '("libavcodec.57.dylib"
+    "libavdevice.57.dylib"
+    "libavfilter.6.dylib"
+    "libavformat.57.dylib"
+    "libavutil.55.dylib"
+    "libswresample.2.dylib"
+    "libswscale.4.dylib"))
