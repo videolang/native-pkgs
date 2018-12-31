d3c79ef0e01617a785201da5c1608a1ecfd6e177
Leif Andersen <leif@leifandersen.net>
2018-12-31 15:56:12 -0500

    debugging, ignore

--- build-lib.rkt
+++ build-lib.rkt
@@ -137,7 +137,7 @@
                                          [(64) "-m64"]))]
                  [(macosx) (list "-undefined" "dynamic_lookup"
                                  "-L../ffmpeg-src/lib/" "-lavutil")]
-                 [(windows) (list* ;"-mwindows"
+                 [(windows) (list* "-mwindows"
                                    (case word-size
                                      [(32) (list "-L../ffmpeg-i386-win32/" "-lavutil-56")]
                                      [(64) (list "-L../ffmpeg-x86_64-win32/" "-lavutil-56")]))])
